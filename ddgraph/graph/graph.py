45b7a6620fe4bbc67b317997655ec42a174ccf10
Ivan Hristov <ivan.hristov.biz@gmail.com>
2022-06-21 09:53:00 +0300

    Add entities to graph representation
    - Would make my life easier in terms of implementing TransE

--- ddgraph/graph/graph.py
+++ ddgraph/graph/graph.py
@@ -5,3 +5,5 @@ class UserItemGraph:
     # item offset is the adj_list length
     adj_list: List[List[Tuple[int, int]]]
     relationships: List[str]
+    user_entities: List[str]
+    item_entities: List[str]
--- ddgraph/graph/movielens.py
+++ ddgraph/graph/movielens.py
@@ -1,5 +1,7 @@
 import re
 from pathlib import Path
+from typing import List, Tuple
+from urllib.parse import uses_fragment
 
 import ddgraph.graph.graph as graph
 
@@ -25,20 +27,32 @@ class MovieLensParser:
     _EXPECTED_UITEM_CHUNKS_PER_LINE = 24
     
     _data_path: Path
-    # The movielens 100k dataset has distinct orders for users and items
-    # but a way is needed to index them in an adjacency matrix and other graph structures.
-    # Thus item_id_offset pushes all item ids after user ids
-    _item_id_offset: int
 
     def __init__(self, data_path: Path) -> None:
         self._data_path = data_path
-        self._item_id_offset = 0
 
-    def parse(self) -> graph.UserItemGraph:    
-        self._item_id_offset = self._num_users()
+    def parse(self) -> graph.UserItemGraph:
+        g = graph.UserItemGraph()
+        g.relationships = [LIKES, DISLIKES]
+        g.user_entities = self._user_entities()
+        g.item_entities = self._item_entities()
+        
+        g.adj_list = self._adj_list(len(g.user_entities))
 
-        udata = self._udata()
-        return self._user_item_graph(udata)
+        return g
+
+    def _user_entities(self) -> str:
+        entities = []
+        
+        for uuser_line in self._uuser().split("\n"):
+            chunks = uuser_line.split(self._PIPE_SPLITTER)
+            
+            if len(chunks) != self._EXPECTED_UUSER_CHUNKS_PER_LINE:
+                continue
+
+            entities.append(f"{chunks[1]}-{chunks[2]}-{chunks[3]}-{chunks[4]}")
+        
+        return entities
 
     def _uuser(self) -> str:
         return self._read_file(self._uuser_path())
@@ -46,39 +60,44 @@ class MovieLensParser:
     def _uuser_path(self) -> str:
         return Path(self._data_path, "u.user")
 
+    def _item_entities(self) -> str:
+        entities = []
+        
+        for uitem_line in self._uitem().split("\n"):
+            chunks = uitem_line.split(self._PIPE_SPLITTER)
+
+            if len(chunks) != self._EXPECTED_UITEM_CHUNKS_PER_LINE:
+                continue
+
+            entities.append(chunks[1])
+        
+        return entities
+
+    def _uitem(self) -> str:
+        return self._read_file(self._uitem_path())
+
+    def _uitem_path(self) -> str:
+        return Path(self._data_path, "u.item")
+
     def _udata(self) -> str:        
         udata = self._read_file(self._udata_path())
 
         return re.sub(r"[^\S\n\r]+", self._UDATA_SEPARATOR, udata)
 
     def _read_file(self, path: Path) -> str:    
-        with open(path) as stream:
+        with open(path, encoding="latin-1") as stream:
             return stream.read()
 
     def _udata_path(self) -> Path:
         return Path(self._data_path, "u.data")
 
-    def _num_users(self) -> int:
-        offset = 0
-        
-        for uuser_line in self._uuser().split("\n"):
-            chunks = uuser_line.split(self._PIPE_SPLITTER)
-            
-            if len(chunks) != self._EXPECTED_UUSER_CHUNKS_PER_LINE:
-                break
-            
-            offset += 1
-        
-        return offset
-
-    def _user_item_graph(self, udata: str) -> graph.UserItemGraph:        
-        g = graph.UserItemGraph()
-        g.relationships = [LIKES, DISLIKES]
-        g.adj_list = []
+    def _adj_list(self, user_count: int) -> List[List[Tuple[int, int]]]:        
+        udata = self._udata()
+        adj_list = []
 
         # TODO: Find better syntactic sugar.
-        for _ in range(self._item_id_offset):
-            g.adj_list.append([])
+        for _ in range(user_count):
+            adj_list.append([])
 
         relationship_indices = {LIKES: 0, DISLIKES: 1}
 
@@ -91,13 +110,13 @@ class MovieLensParser:
             # Indices start from 1 in the dataset but we translate them
             # to an ordering suitable for a normal programming language.
             user_id = int(chunks[self._UDATA_USER_ID_POS]) - 1
-            item_id = self._item_id_offset + int(chunks[self._UDATA_ITEM_ID_POS]) - 1
+            item_id = user_count + int(chunks[self._UDATA_ITEM_ID_POS]) - 1
             rating = int(chunks[self._UDATA_RATING_POS])
             label = self._rating_to_label(rating)
             
-            g.adj_list[user_id].append((relationship_indices[label], item_id))
+            adj_list[user_id].append((relationship_indices[label], item_id))
 
-        return g
+        return adj_list
 
     def _rating_to_label(self, rating: int) -> str:
         if rating >= 4:
